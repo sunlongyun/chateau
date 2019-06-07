25d5163152a84e6906d6d32805a52129378c43ab
longyun.sun <longyun.sun@lianshang.com>
2019-06-07 10:32:55 +0800

    酒庄

---
+++ src/test/java/com/caichao/chateau/ChateauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caichao.chateau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChateauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
