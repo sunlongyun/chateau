17738382776e83303ebbbb32ffd2a6472537f8e0
longyun.sun <longyun.sun@lianshang.com>
2019-06-24 07:30:52 +0800

    代码提交

--- src/main/java/com/caichao/chateau/app/wpay/util/CCWxPayConfig.java
+++ src/main/java/com/caichao/chateau/app/wpay/util/CCWxPayConfig.java
@@ -1,41 +1,41 @@
-package com.caichao.chateau.app.wpay.util;
-
-import com.github.wxpay.sdk.IWXPayDomain;
-import com.github.wxpay.sdk.WXPayConfig;
-import java.io.InputStream;
-
-/**
- * 描述:
- *
- * @AUTHOR 孙龙云
- * @date 2019-06-13 21:35
- */
-public class CCWxPayConfig extends WXPayConfig{
-
-	@Override
-	public String getMchID() {
-		return "1539835521";
-	}
-
-	@Override
-	public String getAppID() {
-		return "wxac4c162ada26ae0e";
-	}
-
-	@Override
-	public String getKey() {
-		return "sunlongyun1478529630sunlongyun12";
-	}
-
-	@Override
-	public InputStream getCertStream() {
-		return null;
-	}
-
-	@Override
-	public IWXPayDomain getWXPayDomain() {
-		return null;
-	}
-
-
-}
+//package com.caichao.chateau.app.wpay.util;
+//
+//import com.github.wxpay.sdk.IWXPayDomain;
+//import com.github.wxpay.sdk.WXPayConfig;
+//import java.io.InputStream;
+//
+///**
+// * 描述:
+// *
+// * @AUTHOR 孙龙云
+// * @date 2019-06-13 21:35
+// */
+//public class CCWxPayConfig extends WXPayConfig{
+//
+//	@Override
+//	public String getMchID() {
+//		return "1539835521";
+//	}
+//
+//	@Override
+//	public String getAppID() {
+//		return "wxac4c162ada26ae0e";
+//	}
+//
+//	@Override
+//	public String getKey() {
+//		return "sunlongyun1478529630sunlongyun12";
+//	}
+//
+//	@Override
+//	public InputStream getCertStream() {
+//		return null;
+//	}
+//
+//	@Override
+//	public IWXPayDomain getWXPayDomain() {
+//		return null;
+//	}
+//
+//
+//}
