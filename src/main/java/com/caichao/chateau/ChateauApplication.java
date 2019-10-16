1d8766ebd644f104c06d5877d71fb42a702a3aa2
longyun.sun <longyun.sun@lianshang.com>
2019-10-16 18:03:12 +0800

    代码提交

--- src/main/java/com/caichao/chateau/ChateauApplication.java
+++ src/main/java/com/caichao/chateau/ChateauApplication.java
@@ -3,11 +3,15 @@ package com.caichao.chateau;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.caichao.chateau.*.mapper")
 @EnableScheduling
+@PropertySource("file:${chateau_path_file}")
 public class ChateauApplication {
 
 	public static void main(String[] args) {
